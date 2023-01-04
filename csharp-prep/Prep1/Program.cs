21b54d1398eadfe7d5ea8ce7c76afaffafcbbe78
Cory Poulson <Xelor41023@gmail.com>
2023-01-04 04:41:19 -0700

    Completed Preperation 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"Input Accepted your first name is {firstName}");
+        Console.WriteLine("What is  your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Input Accepted your last name is {lastName}");
+        Console.WriteLine("Printing Iconic Line");
+        Console.WriteLine($"My Name is {firstName}.... {firstName}{lastName} ");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What Grade did the user recive?");
+        string studentGrade = Console.ReadLine();
+        Console.WriteLine($" Input accepted user recived a grade of {studentGrade}");
+        int numStudentGrade = int.Parse(studentGrade);
+        string studentGradeLetter = "";
+
+        if (numStudentGrade >= 90)
+            {
+                studentGradeLetter = "A";
+            }
+        else if (numStudentGrade >= 80)
+            {
+                studentGradeLetter = "B";
+            }
+        else if (numStudentGrade >= 70)
+            {
+                studentGradeLetter = "C";
+            }
+        else if (numStudentGrade >= 60)
+            {
+                studentGradeLetter = "D";
+            }
+        else if  (numStudentGrade < 60)
+            {
+                studentGradeLetter = "F";
+            }
+        
+        Console.WriteLine($"Student Recived a Grade of {numStudentGrade} and recived a letter of {studentGradeLetter}");
+
+        if (numStudentGrade >70)
+            {
+                Console.WriteLine(" Student has pass the job good job!");
+            }
+        else
+            {
+                Console.WriteLine("Try again I belive in you!");
+            }
+
     }
 }
