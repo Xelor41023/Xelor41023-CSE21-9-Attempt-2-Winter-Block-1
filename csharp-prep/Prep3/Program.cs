1ab5bd1125aa51e30272b4c2850f0151cbbc4953
Cory Poulson <Xelor41023@gmail.com>
2023-01-05 16:55:11 -0700

    Completed Prep 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        
+        bool gameOver = false;
+        string userGuess = "0";
+        int guessCounter  = 0;
+
+        Console.WriteLine(" Im thinking of a number from 1-100 can you guess it?");
+
+        do 
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,100);
+
+            do{
+                
+                userGuess = Console.ReadLine();
+                string playAgainValue = "";
+                int intUserGuess = int.Parse(userGuess);
+                
+                if (magicNumber > intUserGuess)
+                    {
+                        System.Console.WriteLine("Guess it to low try again");
+                        guessCounter ++;
+                    }
+                else if (magicNumber < intUserGuess)
+                    {
+                        System.Console.WriteLine("Guess is to High Try again");
+                        guessCounter++;
+                    }
+                else if (magicNumber == intUserGuess)
+                    {
+                        System.Console.WriteLine(@$"You guessed correct good job the magic number was {magicNumber}
+                        you guesseed it in {guessCounter} Guesses.");
+
+                        System.Console.WriteLine("play again? y or n.");
+                        playAgainValue = Console.ReadLine();
+                        
+                        if (playAgainValue == "n")
+                            {
+                                gameOver = true;
+                            }
+                        else if (playAgainValue == "y")
+                            {
+                                gameOver = false;
+                            }
+
+
+                        
+                        
+                    }
+
+            }while (gameOver == false);
+        }while (newGame == true;)
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+         bool numbersListCompleted = false;
+         string inputAnotherNumberCheck = "n";
+        
+
+        
+
+        do
+        {
+            Console.WriteLine("Enter a number To be used in the calculation");
+            int userInt = int.Parse(Console.ReadLine());
+            numbers.Add(userInt);
+            
+            System.Console.WriteLine(" Enter Another Number? y/n");
+             inputAnotherNumberCheck = Console.ReadLine();
+             
+            
+            if (inputAnotherNumberCheck == "y" )
+                {
+                    numbersListCompleted = false;
+                } 
+            else if (inputAnotherNumberCheck == "n")
+                {
+                    numbersListCompleted = true;
+                }
+           
+
+        }while (numbersListCompleted == false);
+
+        int numbersCount = numbers.Count;
+        int numbersSum = 0;
+        int numbersAverage = 0;
+        int numbersHighest = 0;
+
+        foreach ( int number in numbers)
+         {
+            numbersSum = number + numbersSum;
+            numbersAverage = numbersSum/numbersCount;
+            if (number >= numbersHighest)
+                {
+                    numbersHighest = number;
+                }
+         }
+
+            Console.WriteLine($"The Numbers Sum is {numbersSum}");
+            Console.WriteLine($" The Numbers Average is {numbersAverage}");
+            Console.WriteLine($"The Highest Number is {numbersHighest}");
+            
+         
+        
+
     }
 }
