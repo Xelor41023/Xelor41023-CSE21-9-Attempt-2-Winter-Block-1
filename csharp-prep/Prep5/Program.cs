f21949f873a8078185822251ffd7673f6efd2a01
Cory Poulson <Xelor41023@gmail.com>
2023-01-05 23:52:38 -0700

    Completed Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,13 +5,52 @@ class Program
     static void Main(string[] args)
     {
         WelcomeMessage();
-        
-
+        string fullName = GetUserName();
+        int userNumber = GetUserNumber();
+        int squaredUserNumber = SquareUserNumber(userNumber);
+        DisplayResult(fullName, squaredUserNumber);
     }
-
-    static public void WelcomeMessage();
+    static void WelcomeMessage()
     {
-        System.Console.WriteLine("Welcome to The Program!");
+        Console.WriteLine("Welcome to The Program");
     }
+
+    static string GetUserName()
+    {
+        string firstName = "";
+        string lastName = "";
+        string fullName = "";
+
+        Console.WriteLine("What is your First Name?");
+        firstName = Console.ReadLine();
+        Console.WriteLine($"input accepted your first name is {firstName}");
+        Console.WriteLine("What is your last name?");
+        lastName = Console.ReadLine();
+        Console.WriteLine($"input accepted your last name is {lastName}");
+        fullName = firstName + "" + lastName;
+        return fullName;
+
+    }
+
+    static int GetUserNumber()
+    {
+        int userNumber = 0;
+        System.Console.WriteLine("What is your favorite Number?");
+        userNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine($"input accepted your favorite number is {userNumber}");
+        return userNumber;
+    }
+
+    static int SquareUserNumber( int userNumber)
+    {
+        int squaredUserNumber = userNumber * userNumber;
+        return squaredUserNumber;
+    }
+
+    static void DisplayResult(string fullName, int squaredUserNumber)
+    {
+        Console.WriteLine($"{fullName} the square of your number is  {squaredUserNumber}");
+    }
+
 }
     
